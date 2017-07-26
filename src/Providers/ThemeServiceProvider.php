c0bb75deacc0936adeb5814984aea17bbef2921e
Tomasz Marach <tma@crehler.com>
2017-07-26 12:27:57 +0200

    CrTheme 1.0.0 - background changes

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CrTheme\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+}
