8fbf734a2d5ca9e34128ea7cec317d5f1e36b252
Tomasz Marach <tma@crehler.com>
2017-07-27 09:08:05 +0200

    CrTheme 1.0.5

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -19,18 +19,16 @@ class ThemeServiceProvider extends ServiceProvider
 
     }
 
-    /**
-     * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
-     */
+
     public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
 
         $eventDispatcher->listen('IO.tpl.basket', function(TemplateContainer $container, $templateData)
         {
             $container->setTemplate('CrTheme::content.ThemeBasket');
-            return false;
-        }, 0);
 
+        }, 0);
+        return false;
     }
 
 }
